b215e68e48b9a4bebb402530b1495e983cfddc5b
alexandrejls <57766439+alexandrejls@users.noreply.github.com>
2019-11-18 09:52:58 -0300

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -1,23 +1,8 @@
 #!/bin/bash
-# Autor: Robson Vaamonde
-# Site: www.procedimentosemti.com.br
-# Facebook: facebook.com/ProcedimentosEmTI
-# Facebook: facebook.com/BoraParaPratica
-# YouTube: youtube.com/BoraParaPratica
-# Data de criação: 10/11/2018
-# Data de atualização: 22/10/2019
-# Versão: 0.3
-# Testado e homologado para a versão do Ubuntu Server 18.04.x LTS x64
-# Kernel >= 4.15.x
-#
 # Webmin é um programa de gerenciamento de servidor, que roda em plataformas Unix/Linux. Com ele você pode usar também o 
 # Usermin e o Virtualmin. O Webmin funciona como um centralizador de configurações do sistema, monitoração dos serviços e de
 # servidores, fornecendo uma interface amigável, e que quando configurado com um servidor web, pode ser acessado de qualquer
 # local, através de um navegador: https:\\(ip do servidor):(porta de utilização). Exemplo: https:\\172.16.1.20:10000
-#
-# Vídeo de instalação do GNU/Linux Ubuntu Server 18.04.x LTS: https://www.youtube.com/watch?v=zDdCrqNhIXI
-# Vídeo de instalação do LAMP Server no Ubuntu Server 18.04.x LTS: https://www.youtube.com/watch?v=6EFUu-I3u4s
-# Vídeo de instalação do Wordpress no LAMP do Ubuntu Server 18.04.x LTS: https://www.youtube.com/watch?v=Fs2B7kLdlm4
 #
 # Variável da Data Inicial para calcular o tempo de execução do script (VARIÁVEL MELHORADA)
 # opção do comando date: +%T (Time)
@@ -30,50 +15,33 @@ HORAINICIAL=`date +%T`
 # opção do shell script: acento crase ` ` = Executa comandos numa subshell, retornando o resultado
 # opção do shell script: aspas simples ' ' = Protege uma string completamente (nenhum caractere é especial)
 # opção do shell script: aspas duplas " " = Protege uma string, mas reconhece $, \ e ` como especiais
-USUARIO=`id -u`
-UBUNTU=`lsb_release -rs`
-KERNEL=`uname -r | cut -d'.' -f1,2`
+#USUARIO=`id -u`
+#UBUNTU=`lsb_release -rs`
+#KERNEL=`uname -r | cut -d'.' -f1,2`
 #
 # Variável do caminho do Log dos Script utilizado nesse curso (VARIÁVEL MELHORADA)
 # opções do comando cut: -d (delimiter), -f (fields)
 # $0 (variável de ambiente do nome do comando)
 LOG="/var/log/$(echo $0 | cut -d'/' -f2)"
 #
-# Variável do download do Webmin - (Link de download atualizado em: 20/01/2019)
-WEBMIN="http://prdownloads.sourceforge.net/webadmin/webmin_1.930_all.deb"
-#
-# Verificando se o usuário e Root, Distribuição e >=18.04 e o Kernel >=4.15 <IF MELHORADO)
-# [ ] = teste de expressão, && = operador lógico AND, == comparação de string, exit 1 = A maioria dos erros comuns na execução
-clear
-if [ "$USUARIO" == "0" ] && [ "$UBUNTU" == "18.04" ] && [ "$KERNEL" == "4.15" ]
-	then
-		echo -e "O usuário e Root, continuando com o script..."
-		echo -e "Distribuição e >=18.04.x, continuando com o script..."
-		echo -e "Kernel e >= 4.15, continuando com o script..."
-		sleep 5
-	else
-		echo -e "Usuário não e Root ($USUARIO) ou Distribuição não e >=18.04.x ($UBUNTU) ou Kernel não e >=4.15 ($KERNEL)"
-		echo -e "Caso você não tenha executado o script com o comando: sudo -i"
-		echo -e "Execute novamente o script para verificar o ambiente."
-		exit 1
-fi
-#
-# Script de instalação do Webmin no GNU/Linux Ubuntu Server 18.04.x
 # opção do comando echo: -e (enable interpretation of backslash escapes), \n (new line)
 # opção do comando hostname: -I (all IP address)
 # opção do comando date: + (format), %d (day), %m (month), %Y (year 1970), %H (hour 24), %M (minute 60)
 echo -e "Início do script $0 em: `date +%d/%m/%Y-"("%H:%M")"`\n" &>> $LOG
 clear
 #
-echo -e "Instalação do Webmin no GNU/Linux Ubuntu Server 18.04.x\n"
+echo deb https://download.webmin.com/download/repository sarge contrib >> /etc/apt/sources.list
 echo -e "Após a instalação do Webmin acessar a URL: https://`hostname -I`:10000/\n"
 echo -e "Aguarde, esse processo demora um pouco dependendo do seu Link de Internet..."
 sleep 5
 echo
 #
-echo -e "Adicionando o Repositório Universal do Apt, aguarde..."
+echo -e "Adicionando o Repositório e Assinatura, aguarde..."
 	# opção do comando: &>> (redirecionar a saída padrão)
-	add-apt-repository universe &>> $LOG
+	cd /root
+	wget http://www.webmin.com/jcameron-key.asc  &>> $LOG
+	apt-key add jcameron-key.asc  &>> $LOG
+	apt-get install apt-transport-https &>> $LOG
 echo -e "Repositório adicionado com sucesso!!!, continuando com o script..."
 sleep 5
 echo
@@ -107,25 +75,8 @@ echo
 echo -e "Instalando as dependências do Webmin, aguarde..."
 	# opção do comando: &>> (redirecionar a saída padrão)
 	# opção do comando apt: -y (yes), \ (Bar, opção de quebra de linha no apt)
-	apt -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions \
-	python unzip &>> $LOG
+	apt-get install webmin &>> $LOG
 echo -e "Instalação das dependências feita com sucesso!!!, continuando com o script..."
-sleep 5
-echo
-#
-echo -e "Fazendo o download do Webmin, aguarde..."
-	# opção do comando: &>> (redirecionar a saída padrão)
-	wget $WEBMIN &>> $LOG
-echo -e "Download do Webmin feito com sucesso!!!, continuando com o script..."
-sleep 5
-echo
-#				 
-echo -e "Instalando o Webmin, aguarde..."
-	# opção do comando: &>> (redirecionar a saída padrão)
-	# opção do comando dpkg: -i (install)
-	WEBMINVERSION=`echo webmin_*`
-	dpkg -i $WEBMINVERSION &>> $LOG
-echo -e "Instalação do Webmin feita com sucesso!!!, continuando com o script..."
 sleep 5
 echo
 #
