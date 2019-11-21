b7ea1ef551a09048343e2e118cebd0d0ef4256b2
alexandrejls <57766439+alexandrejls@users.noreply.github.com>
2019-11-21 20:55:30 -0300

    Update webmin.sh

--- webmin.sh
+++ webmin.sh
@@ -40,7 +40,8 @@ echo -e "Adicionando o Repositório e Assinatura, aguarde..."
 	# opção do comando: &>> (redirecionar a saída padrão)
 	cd /root
 	wget http://www.webmin.com/jcameron-key.asc  &>> $LOG
-	apt-get install -y perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python &>> $LOG
+	apt-get install -y perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime &>> $LOG
+	apt-get install -y libio-pty-perl apt-show-versions python &>> $LOG
 	apt-get install -y gnupg gnupg2 &>>$LOG
 	apt-key add jcameron-key.asc  &>> $LOG
 	apt-get install -y apt-transport-https &>> $LOG
